eb9946d8cf2a614b171dc6b07e9f05ca54407840
Gregory Siems <gsiems@gmail.com>
2025-03-15 16:26:07 -0500

    Convert from plpgslq function to sql function. Add search_path.

--- schema/util_log/function/log_atx.sql
+++ schema/util_log/function/log_atx.sql
@@ -6,9 +6,10 @@ CREATE OR REPLACE FUNCTION util_log.log_atx (
     a_obj_name text,
     a_calling_obj_name text,
     a_remarks text )
-RETURNS VOID
-LANGUAGE plpgsql
+RETURNS void
+LANGUAGE SQL
 SECURITY DEFINER
+SET search_path = pg_catalog, util_log
 AS $$
 /**
 Function log_atx inserts into the dt_proc_log table and is intended to
@@ -26,40 +27,38 @@ in the event of rollbacks in the data processing
 | a_remarks                      | in     | text       | The remarks/text to be logged                      |
 
 */
-BEGIN
 
-    INSERT INTO util_log.dt_proc_log (
-            date_exec,
-            tmsp_exec,
-            client_address,
-            client_port,
-            pid,
-            log_level,
-            obj_line_number,
-            calling_obj_line_number,
-            db_name,
-            username,
-            application_name,
-            obj_name,
-            calling_obj_name,
-            remarks )
-        SELECT current_date AS date_exec,
-                current_timestamp AS tmsp_exec,
-                psa.client_addr,
-                psa.client_port,
-                psa.pid,
-                a_log_level AS log_level,
-                a_obj_line_number AS obj_line_number,
-                a_calling_obj_line_number AS calling_obj_line_number,
-                psa.datname AS db_name,
-                psa.usename AS username,
-                psa.application_name,
-                a_obj_name AS obj_name,
-                a_calling_obj_name AS calling_obj_name,
-                a_remarks AS remarks
-            FROM pg_stat_activity psa
-            WHERE psa.pid = a_pid
-            LIMIT 1 ;
+INSERT INTO util_log.dt_proc_log (
+        date_exec,
+        tmsp_exec,
+        client_address,
+        client_port,
+        pid,
+        log_level,
+        obj_line_number,
+        calling_obj_line_number,
+        db_name,
+        username,
+        application_name,
+        obj_name,
+        calling_obj_name,
+        remarks )
+    SELECT current_date AS date_exec,
+            current_timestamp AS tmsp_exec,
+            psa.client_addr,
+            psa.client_port,
+            psa.pid,
+            a_log_level AS log_level,
+            a_obj_line_number AS obj_line_number,
+            a_calling_obj_line_number AS calling_obj_line_number,
+            psa.datname AS db_name,
+            psa.usename AS username,
+            psa.application_name,
+            a_obj_name AS obj_name,
+            a_calling_obj_name AS calling_obj_name,
+            a_remarks AS remarks
+        FROM pg_stat_activity psa
+        WHERE psa.pid = a_pid
+        LIMIT 1 ;
 
-END ;
 $$ ;
