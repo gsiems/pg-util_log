97f2b6730ab6ccdac1ced545ba34d9dd60209d46
Gregory Siems <gsiems@gmail.com>
2025-11-13 08:44:51 -0600

    minor formatting

--- schema/04_create_database.sql
+++ schema/04_create_database.sql
@@ -1,7 +1,7 @@
 SET statement_timeout = 0 ;
 SET client_encoding = 'UTF8' ;
-SET standard_conforming_strings = on ;
-SET check_function_bodies = true ;
+SET standard_conforming_strings = ON ;
+SET check_function_bodies = TRUE ;
 SET client_min_messages = warning ;
 
 DROP DATABASE IF EXISTS util_log ;
