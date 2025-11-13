88de71c2dfbe38ec904015536b3ecb6653d28a3b
Gregory Siems <gsiems@gmail.com>
2025-11-13 08:45:44 -0600

    Create role nologin. minor formatting

--- schema/03_create_roles.sql
+++ schema/03_create_roles.sql
@@ -1,15 +1,14 @@
 SET statement_timeout = 0 ;
 SET client_encoding = 'UTF8' ;
-SET standard_conforming_strings = on ;
-SET check_function_bodies = true ;
+SET standard_conforming_strings = ON ;
+SET check_function_bodies = TRUE ;
 SET client_min_messages = warning ;
 
 \unset ON_ERROR_STOP
 
 DROP ROLE util_log_tester ;
 
-CREATE ROLE util_log_tester LOGIN PASSWORD 'util_log_tester'
-    NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION ;
+CREATE ROLE util_log_tester NOLOGIN NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION ;
 
 COMMENT ON ROLE util_log_tester IS 'Role for testing util_log functionality' ;
 
